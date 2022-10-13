9d9aaa6c08a5935fe25bbfd968adee2c716348ed
kom50 <omprakashkartik82280@gmail.com>
2022-10-13 23:56:30 +0530

    Add public

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url'
+
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('public'))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../public/index.html'))
+})
 
 app.get('/api', (req, res) => {
     res.json({
