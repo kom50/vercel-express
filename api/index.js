4bd2154b644e8041c78ed499236486a8e906ccc0
kom50 <omprakashkartik82280@gmail.com>
2022-10-13 23:43:38 +0530

    Add public

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
 const app = express();
-const PORT = 5000
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('public'))
 
 app.get('/api', (req, res) => {
     res.json({
